5406cc4a7de47f64ce69b5133d51bd9cd3214c46
kwahlin <kalle.wahlin@kb.se>
2021-11-10 15:08:04 +0100

    Start examine partOfPlace paths

--- whelktool/scripts/analysis/lxl-2483-wikidata-places/script.groovy
+++ whelktool/scripts/analysis/lxl-2483-wikidata-places/script.groovy
@@ -23,13 +23,13 @@ Map testData = geoClassData.findAll { uri, data ->
     data.members > 1000 || data.membersInSweden > 200
 }
 
-//addAvgPartOfRelations(testData)
+addAvgPartOfRelations(testData)
 
-//testData.each { uri, data ->
-//    geoClassStats.println("${uri}\t${data.label}\t${data.members}\t${data.membersInSweden}\t${data.avgPartOfRelations}")
-//}
+testData.each { uri, data ->
+    geoClassStats.println("${uri}\t${data.label}\t${data.members}\t${data.membersInSweden}\t${data.avgPartOfRelations}")
+}
 
-//addPartOfPathLengthData(testData)
+//addPartOfPathData(testData)
 
 //printExamples()
 
@@ -87,32 +87,124 @@ void addMembersInSweden(Map classData) {
 }
 
 void addAvgPartOfRelations(Map classData) {
-    int count = 0
     classData.each { uri, data ->
         String queryString = """
-            SELECT (avg(?partOfCount) as ?avg) {
-                SELECT ?member (count(?place) as ?partOfCount) {
-                    ?member wdt:${WikidataEntity.INSTANCE_OF} <${uri}> ;
-                            p:${WikidataEntity.PART_OF_PLACE} ?partOfStmt .
-                    ?partOfStmt ps:${WikidataEntity.PART_OF_PLACE} ?place .
-                    FILTER NOT EXISTS { ?partOfStmt pq:${WikidataEntity.END_TIME} ?endTime }
-                }
-                GROUP BY ?member
+            SELECT ?member (count(?place) as ?partOfCount) {
+                ?member wdt:${WikidataEntity.INSTANCE_OF} <${uri}> ;
+                        p:${WikidataEntity.PART_OF_PLACE} ?partOfStmt .
+                ?partOfStmt ps:${WikidataEntity.PART_OF_PLACE} ?place .
+                FILTER NOT EXISTS { ?partOfStmt pq:${WikidataEntity.END_TIME} ?endTime }
             }
+            GROUP BY ?member
         """
 
         ResultSet rs = QueryRunner.remoteSelectResult(queryString, WikidataEntity.WIKIDATA_ENDPOINT)
 
-        count += 1
-        println(count)
-
-        if (!rs.hasNext()) {
-            println("No result")
-            return
+        int total = 0
+        rs.each {
+            int count = it.get('partOfCount').getInt()
+            incrementStats("Total", "partOf: ${count}")
+            incrementStats(data.label, "partOf: ${count}", it.get('member').toString())
+            total += count
         }
 
-        QuerySolution singleRowRes = rs.next()
+        data['avgPartOfRelations'] = total / data.members
 
-        data['avgPartOfRelations'] = singleRowRes.get('avg').getFloat()
+        (rs.size()..<data.members).each {
+            incrementStats("Total", "partOf: 0")
+            incrementStats(data.label, "partOf: 0")
+        }
+
+        println(data.label)
+        println(total / data.members)
     }
 }
+
+//void addPartOfPathData(Map classData) {
+//    classData.each { uri, data ->
+//        int minStepsTotal = 0
+//        int maxStepsTotal = 0
+//        int stepsTotal = 0
+//        int pathsTotal = 0
+//        int pathExistsForCountry = 0
+//        int pathExistsForEntity = 0
+//
+//        List<String> classMembers = getClassMembers(uri)
+//        classMembers.each {
+//            WikidataEntity placeEntity = new WikidataEntity(uri)
+//
+//            List country = placeEntity.getCountry()
+//
+//            incrementStats("Total", "country: ${country.size()}", it)
+//            incrementStats(data.label, "country: ${country.size()}", it)
+//
+//            boolean pathExists
+//            country.each {
+//                List paths = pathsToCountry(placeEntity, it.toString())
+//                int pathsCount = paths.size()
+//                if (pathsCount > 0) {
+//                    pathExists = true
+//                    pathExistsForCountry += 1
+//
+//                    pathsTotal += pathsCount
+//                    incrementStats("Total", "Different paths to country: ${pathsCount}", placeEntity.entityIri)
+//                    incrementStats(data.label, "Different paths to country: ${pathsCount}", placeEntity.entityIri)
+//
+//                    int minSteps = paths.min { it.size() } - 1
+//                    minStepsTotal += minSteps
+//                    incrementStats("Total", "Min steps to country: ${minSteps}", placeEntity.entityIri)
+//                    incrementStats(data.label, "Min steps to country: ${minSteps}", placeEntity.entityIri)
+//
+//                    int maxSteps = paths.max { it.size() } - 1
+//                    maxStepsTotal += maxSteps
+//                    incrementStats("Total", "Max steps to country: ${maxSteps}", placeEntity.entityIri)
+//                    incrementStats(data.label, "Max steps to country: ${maxSteps}", placeEntity.entityIri)
+//
+//                    stepsTotal += paths.flatten().size() - pathsCount
+//                }
+//            }
+//            if (pathExists)
+//                pathExistsForEntity += 1
+//        }
+//
+//        data['pathExists'] = pathExistsForEntity
+//        data['avgDifferentPaths'] = pathsTotal / pathExistsForCountry
+//        data['avgSteps'] = stepsTotal / pathsTotal
+//        data['avgMinSteps'] = minStepsTotal / pathExistsForCountry
+//        data['avgMaxSteps'] = maxStepsTotal / pathExistsForCountry
+//    }
+//}
+//
+//List<String> getClassMembers(String uri) {
+//    String queryString = "SELECT ?member { ?member wdt:${WikidataEntity.INSTANCE_OF} <${uri}>"
+//
+//    ResultSet rs = QueryRunner.remoteSelectResult(queryString, WikidataEntity.WIKIDATA_ENDPOINT)
+//
+//    return rs.collect { it.get('member').toString() }
+//}
+//
+//List pathsToCountry(WikidataEntity place, String countryUri) {
+//    List allPaths = []
+//
+//    List path = [place]
+//    Queue q = [path] as Queue
+//
+//    while (!q.isEmpty()) {
+//        path = q.poll()
+//        WikidataEntity lastInPath = path.last()
+//
+//        if (lastInPath.entityIri == countryUri) {
+//            allPaths << path
+//            continue
+//        }
+//
+//        lastInPath.getPartOfPlace().each {
+//            String uri = it.toString()
+//            if (path.any { it.entityIri == uri })
+//                return
+//            q << path + new WikidataEntity(uri)
+//        }
+//    }
+//
+//    return allPaths
+//}
